2d3a94ec491706f1264e708b3218be40ae5ea3e4
vahnzoel <vahnzoel@gmail.com>
2021-05-20 06:02:45 +0700

    Update Database.php

--- Website/E-SPJ/app/Config/Database.php
+++ Website/E-SPJ/app/Config/Database.php
@@ -32,10 +32,10 @@ class Database extends Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'hostname' => '202.52.146.120',
+		'username' => 'brainweb_jyt',
+		'password' => '@Jyt2021',
+		'database' => 'brainweb_spj_jyt',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
@@ -85,8 +85,7 @@ class Database extends Config
 		// Ensure that we always set the database group to 'tests' if
 		// we are currently running an automated test suite, so that
 		// we don't overwrite live data on accident.
-		if (ENVIRONMENT === 'testing')
-		{
+		if (ENVIRONMENT === 'testing') {
 			$this->defaultGroup = 'tests';
 		}
 	}
